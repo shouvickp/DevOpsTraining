d0d314653f1a07b25a2d8f87805d3aa5059dad43
Shouvick Paul <shouvickp@gmail.com>
2025-10-04 10:31:12 +0530

    assignment 2 GradeChecker.py

---
+++ PythonAndBash/GradeChecker.py
@@ -0,0 +1,13 @@
+score=float(input("Enter your score: "))
+grade=""
+if score >= 90:
+    grade="A"
+elif score >= 80 and score <=89:
+    grade="B"
+elif score >= 70 and score <=79:
+    grade="C"
+elif score >= 60 and score <=69:
+    grade="D"
+else:
+    grade="F"
+print("Your grade is : " + grade)
